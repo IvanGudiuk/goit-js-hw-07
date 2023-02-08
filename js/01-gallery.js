023e6a0deea1bcf3a6ad08a52de6ba5726915919
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-02-08 22:25:13 +0300

    home task-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemsCreate = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+  })
+  .join(" ");
+
+galleryContainer.innerHTML = galleryItemsCreate;
+
+const onImageClick = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+  else {
+    const targetLink = event.target.dataset.source;
+    basicLightbox
+      .create(
+        `
+  	<img width="1400" height="900" src="${targetLink}">
+  `
+      )
+      .show();
+  }
+};
+galleryContainer.addEventListener("click", onImageClick);
